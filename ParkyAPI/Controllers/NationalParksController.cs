4d75afcf279ec6a42eb55545fd1c9640d6196c48
Sujay Chand <sujaygchand@gmail.com>
2021-07-13 00:48:48 +1200

    Created a national parks controller and tested get all parks thru postman

---
+++ ParkyAPI/Controllers/NationalParksController.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using ParkyAPI.Model;
+using ParkyAPI.Model.DTOs;
+using ParkyAPI.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class NationalParksController : Controller
+	{
+		private readonly INationalParkRepository _nationalParkRepository;
+		private readonly IMapper _mapper;
+
+		public NationalParksController(INationalParkRepository nationalParkRepository, IMapper mapper)
+		{
+			_nationalParkRepository = nationalParkRepository;
+			_mapper = mapper;
+		}
+
+		[HttpGet]
+		public IActionResult GetNationalParks()
+		{
+			ICollection<NationalPark> parkList = _nationalParkRepository?.GetNationalParks();
+
+			if (parkList == null || _mapper == null)
+				return NotFound();
+
+			List<NationalParkDto> parkDtos = new List<NationalParkDto>();
+
+			foreach(var park in parkList)
+			{
+				parkDtos.Add(_mapper.Map<NationalParkDto>(park));
+			}
+
+			return Ok(parkDtos);
+		}
+	}
+}
