cda0e1caca7cabfa7636b630321261fc127179e5
Sujay Chand <sujaygchand@gmail.com>
2021-07-12 22:08:36 +1200

    Added repo interface

---
+++ ParkyAPI/Repository/IRepository/INationalParkRepository.cs
@@ -0,0 +1,20 @@
+﻿using ParkyAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyAPI.Repository.IRepository
+{
+	interface INationalParkRepository
+	{
+		ICollection<NationalPark> GetNationalParks();
+		NationalPark GetNationalPark(int nationalParkId);
+		bool NationalParkExists(string name);
+		bool NationalParkExists(int id);
+		bool TryCreateNationalPark(NationalPark nationalPark);
+		bool TryUpdateNationalPark(NationalPark nationalPark);
+		bool TryDeleteNationalPark(NationalPark nationalPark);
+		bool Save();
+	}
+}
---
+++ ParkyAPI/Repository/NationalParkRepository.cs
@@ -0,0 +1,52 @@
+﻿using ParkyAPI.Model;
+using ParkyAPI.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyAPI.Repository
+{
+	public class NationalParkRepository : INationalParkRepository
+	{
+		public NationalPark GetNationalPark(int nationalParkId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public ICollection<NationalPark> GetNationalParks()
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool NationalParkExists(string name)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool NationalParkExists(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool Save()
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool TryCreateNationalPark(NationalPark nationalPark)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool TryDeleteNationalPark(NationalPark nationalPark)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool TryUpdateNationalPark(NationalPark nationalPark)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
