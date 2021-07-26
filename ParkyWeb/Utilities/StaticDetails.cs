ec4b040f1d43244683f0b6fb11ff57ba8b97c634
Sujay Chand <sujaygchand@gmail.com>
2021-07-26 16:30:25 +1200

    Static details for linking to API

---
+++ ParkyWeb/Utilities/StaticDetails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyWeb.Utilities
+{
+	public static class StaticDetails
+	{
+		public static string APIBaseUrl = "https://localhost:44358/";
+		public static string NationalParkAPIPath = APIBaseUrl + "api/v1/nationalparks";
+		public static string TrailAPIPath = APIBaseUrl + "api/v1/trails";
+	}
+}
