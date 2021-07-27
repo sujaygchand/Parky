e9083db9afd847976d11dbcba7c878f998a608ce
Sujay Chand <sujaygchand@gmail.com>
2021-07-28 00:25:54 +1200

    Made national parks controller and index

---
+++ ParkyWeb/Controllers/NationalParksController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ParkyWeb.Models;
+using ParkyWeb.Repository.IRepository;
+using ParkyWeb.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyWeb.Controllers
+{
+	public class NationalParksController : Controller
+	{
+		public readonly INationalParkRepository _nationalParkRepository;
+
+		public NationalParksController(INationalParkRepository nationalParkRepository)
+		{
+			_nationalParkRepository = nationalParkRepository;
+		}
+
+		public IActionResult Index()
+		{
+			return View(new NationalPark() { });
+		}
+
+		public async Task<IActionResult> GetAllNationalPark()
+		{
+			return Json(new { data = await _nationalParkRepository.GetAllAsync(StaticDetails.NationalParkAPIPath) });
+		}
+	}
+}
